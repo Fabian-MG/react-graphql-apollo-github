7a07444d26fa4695ab39cb96f4cfacaee93f7bca
Fabian-MG <mirandafabian1510@gmail.com>
2020-06-30 23:39:39 -0500

    Updating dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
 
-import './style.css';
+import "./style.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
